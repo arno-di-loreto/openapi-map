99a43e98a544d72db419ae0aa82b7a02fd4a57e2
arno-di-loreto <arno.di.loreto@gmail.com>
2015-10-29 22:53:45 +0100

    updated dist

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -16,10 +16,11 @@ this["SwaggerSpecificationVisualDocumentation"]["tooltip"] = Handlebars.template
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.closedChildren : depth0),{"name":"if","hash":{},"fn":container.program(8, data, 0),"inverse":container.program(10, data, 0),"data":data})) != null ? stack1 : "")
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.gfm : depth0),{"name":"if","hash":{},"fn":container.program(15, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.allowExtension : depth0),{"name":"if","hash":{},"fn":container.program(17, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.swaggerType : depth0),{"name":"if","hash":{},"fn":container.program(19, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "  </div>\n    <h2>Description</h2>\n    <p>"
     + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "</p>\n\n"
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.values : depth0),{"name":"if","hash":{},"fn":container.program(19, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.values : depth0),{"name":"if","hash":{},"fn":container.program(21, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"4":function(container,depth0,helpers,partials,data) {
     return "      <p class=\".list-group-item\"><i class=\"fa fa-asterisk fa-fw fa-2x\"></i>&nbsp;Required</p>\n";
 },"6":function(container,depth0,helpers,partials,data) {
@@ -51,13 +52,19 @@ this["SwaggerSpecificationVisualDocumentation"]["tooltip"] = Handlebars.template
 },"17":function(container,depth0,helpers,partials,data) {
     return "      <p class=\".list-group-item\"><i class=\"fa fa-code-fork fa-fw fa-2x\"></i>&nbsp;Allows extension</p>\n";
 },"19":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "      <p class=\".list-group-item\"><i class=\"fa fa-external-link fa-fw fa-2x\"></i>&nbsp;<a href=\""
+    + container.escapeExpression(((helper = (helper = helpers.swaggerTypeURL || (depth0 != null ? depth0.swaggerTypeURL : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"swaggerTypeURL","hash":{},"data":data}) : helper)))
+    + "\" target=\"_blank\">Swagger documentation</a></p>\n";
+},"21":function(container,depth0,helpers,partials,data) {
     var stack1, helper, options, buffer = 
   "      <h2>Values</h2>\n      <table id=\"table\" class=\"table table-striped table-bordered table-condensed table-hover\" >\n        <tr>\n            <th>\n                Value\n            </th>\n            <th>\n                Default\n            </th>\n            <th>\n                Description\n            </th>\n        </tr>\n\n";
-  stack1 = ((helper = (helper = helpers.values || (depth0 != null ? depth0.values : depth0)) != null ? helper : helpers.helperMissing),(options={"name":"values","hash":{},"fn":container.program(20, data, 0),"inverse":container.noop,"data":data}),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},options) : helper));
+  stack1 = ((helper = (helper = helpers.values || (depth0 != null ? depth0.values : depth0)) != null ? helper : helpers.helperMissing),(options={"name":"values","hash":{},"fn":container.program(22, data, 0),"inverse":container.noop,"data":data}),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},options) : helper));
   if (!helpers.values) { stack1 = helpers.blockHelperMissing.call(depth0,stack1,options)}
   if (stack1 != null) { buffer += stack1; }
   return buffer + "      </table>\n";
-},"20":function(container,depth0,helpers,partials,data) {
+},"22":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
   return "        <tr>\n          <td>"
