c9c15c10a57b6ce09afca7efeb51800507967511
arno-di-loreto <arno.di.loreto@gmail.com>
2018-03-09 10:59:11 +0100

    version url parameter and replaced pointer icon by hand in help

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function loadVersionFile(complete) {
   $.getJSON('spec-versions.json')
   .then(complete)
@@ -59,6 +69,30 @@ function showHome() {
   $('#help').show();
 }
 
+function getVersionById(id, versions){
+  var result;
+  for (var i = 0; i < versions.length; i++) {
+    if(versions[i].id === id) {
+      result = versions[i];
+      break;
+    } 
+  }
+  return result;
+}
+
+function getVersion(versions, url) {
+  var id = getParameterByName('version', url);
+  var version;
+  if(id !== undefined && id !== null && id.length > 0) {
+    version = getVersionById(id, versions);
+  }
+  if(!version){
+    version = versions[0];
+  }
+  return version;
+}
+
 populateVersionList(function(versions) {
-  showVersion(versions[0].name, versions[0].url, versions[0].root, versions[0].specificationurl);
+  var version = getVersion(versions);
+  showVersion(version.name, version.url, version.root, version.specificationurl);
 });
