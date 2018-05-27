35eb0756e3cb4a4acdb8c58a51a41b2e67c99490
arno-di-loreto <arno.di.loreto@gmail.com>
2018-05-27 11:25:16 +0200

    Fix deploy error when no change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -84,10 +84,8 @@ function prepare {
 function publish {
   echo "publishing"
   cd $DEPLOY_TARGET
-  TO_COMMIT=`git status --porcelain | wc -l`
-  if [ "$TO_COMMIT" -eq "0" ]; then
-    echo "Nothing to commit on $DEPLOY_BRANCH in repo $DEPLOY_REPO";
-  else
+  if [ `git status -s | wc -l` -gt 0 ]
+  then
     if [ ${GH_USER+x} ]
     then
       echo "setting git user.name with GH_USER env variable" 
@@ -98,9 +96,11 @@ function publish {
       echo "setting git user.email with GH_EMAIL env variable"
       git config --global user.email "$GH_EMAIL"
     fi
-    git add --all
+    git add .
     git commit -m "$PUBLISH_MESSAGE"
     git push $DEPLOY_REPO_CREDENTIALS
+  else
+    echo "WARN: no change to add to commit"
   fi
 }
 
